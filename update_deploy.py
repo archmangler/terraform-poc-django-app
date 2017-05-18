de8b345c250cb1db028ee3d35c6ae9bad9ddf140
Traiano <twelcome@noon.com>
2017-05-18 19:59:46 +0400

    repo change monitor script with gitpython

--- update_deploy.py
+++ update_deploy.py
@@ -6,9 +6,18 @@
 #
 import subprocess
 import git
+from git import Repo
 
-def git_pull():
+def git_pull(git_dir):
  g = git.cmd.Git(git_dir) 
  g.pull()
 
-git_pull()
+git_dir="/Users/traianowelcome/Desktop/output/dcos_deployers/generic-autoscaling-server/autoscaling/sanity"
+repo = Repo(git_dir)
+assert not repo.bare
+diff = repo.git.diff('HEAD~1..HEAD', name_only=True)
+if(diff):
+ print(diff)
+ git_pull(git_dir)
+else:
+ print("No changes.")
